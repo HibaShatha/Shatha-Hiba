a3d6d810b7aedf831ed6370ef5f1b7e00dda68a1
hibai <hibai@DESKTOP-OOH0I18>
2025-10-13 19:14:27 +0300

    Observer Pattern

--- src/main/java/backend/model/User.java
+++ src/main/java/backend/model/User.java
@@ -4,14 +4,17 @@ public class User {
     private String username;
     private String password;
     private String email;
+    private String phoneNumber;
 
-    public User(String username, String password, String email) {
+    public User(String username, String password, String email, String phoneNumber) {
         this.username = username;
         this.password = password;
         this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 
     public String getUsername() { return username; }
     public String getPassword() { return password; }
     public String getEmail() { return email; }
+    public String getPhoneNumber() { return phoneNumber; }
 }
