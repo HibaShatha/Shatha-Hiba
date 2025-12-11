84c78f5c668558b4e8fc27d7fd2c19987da9029c
Shatha Dwikat <sdwikat93@gmail.com>
2025-12-11 14:13:49 +0200

    test3

--- src/test/java/Strategy/LibrarianTest.java
+++ src/test/java/Strategy/LibrarianTest.java
@@ -1,5 +1,6 @@
 package Strategy;
 
+
 import backend.model.Book;
 import backend.model.CD;
 import backend.model.Librarian;
@@ -14,94 +15,79 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibrarianTest {
+	 static class FakeBookRepo extends BookRepository {
+	        List<Book> books = new ArrayList<>();
+	        public List<Book> getAllBooks() { return books; }
+	    }
 
-    // Fake Repositories
-    static class FakeBookRepo extends BookRepository {
-        List<Book> books = new ArrayList<>();
-        @Override
-        public List<Book> getAllBooks() { return books; }
-    }
+	    static class FakeCDRepo extends CDRepository {
+	        List<CD> cds = new ArrayList<>();
+	        public List<CD> getAllCDs() { return cds; }
+	    }
 
-    static class FakeCDRepo extends CDRepository {
-        List<CD> cds = new ArrayList<>();
-        @Override
-        public List<CD> getAllCDs() { return cds; }
-    }
+	    static class FakeFineRepo extends FineRepository {
+	        Map<String, Double> balance = new HashMap<>();
+	        public double getFineBalance(String username) {
+	            return balance.getOrDefault(username, 0.0);
+	        }
+	        public void updateFineBalance(String username, double newBalance) {
+	            balance.put(username, newBalance);
+	        }
+	    }
 
-    static class FakeFineRepo extends FineRepository {
-        Map<String, Double> balance = new HashMap<>();
-        @Override
-        public double getFineBalance(String username) {
-            return balance.getOrDefault(username, 0.0);
-        }
-        @Override
-        public void updateFineBalance(String username, double newBalance) {
-            balance.put(username, newBalance);
-        }
-    }
+	    @Test
+	    void testUpdateOverdueFines_RealBookAndCD() {
+	        FakeBookRepo bookRepo = new FakeBookRepo();
+	        FakeCDRepo cdRepo = new FakeCDRepo();
+	        FakeFineRepo fineRepo = new FakeFineRepo();
 
-    @Test
-    void testUpdateOverdueFines_AllCases() {
-        FakeBookRepo bookRepo = new FakeBookRepo();
-        FakeCDRepo cdRepo = new FakeCDRepo();
-        FakeFineRepo fineRepo = new FakeFineRepo();
+	        // we will create real Book & CD objects
+	        Book book1 = new Book("Java", "Schildt", "111");
+	        book1.borrow("user1");
 
-        // حالة كتب/CDs متأخرة
-        Book overdueBook = new Book("Java", "Schildt", "111");
-        overdueBook.borrow("user1");
-        overdueBook.dueDate = LocalDate.now().minusDays(5); // overdue 5 days
+	        // نعدل dueDate عشان يكون متأخر
+	        book1.dueDate = LocalDate.now().minusDays(10); // delayed 10 days
 
-        CD overdueCD = new CD("Metallica", "Metallica");
-        overdueCD.borrow("user2");
-        overdueCD.dueDate = LocalDate.now().minusDays(3); // overdue 3 days
+	        Book book2 = new Book("Networks", "Kurose", "222");
+	        book2.borrow("user1");
+	        // غير متأخر
+	        book2.dueDate = LocalDate.now().plusDays(5);
 
-        // حالة كتب/CDs غير متأخرة
-        Book onTimeBook = new Book("Python", "Lutz", "222");
-        onTimeBook.borrow("user3");
-        onTimeBook.dueDate = LocalDate.now().plusDays(2);
 
-        CD onTimeCD = new CD("Linkin Park", "LP");
-        onTimeCD.borrow("user4");
-        onTimeCD.dueDate = LocalDate.now().plusDays(1);
+	        CD cd1 = new CD("Metallica", "Metallica");
+	        cd1.borrow("user2");
+	        cd1.dueDate = LocalDate.now().minusDays(4); // متأخر 4 أيام
 
-        // حالة كتب/CDs بدون مستعير
-        Book unborrowedBook = new Book("C#", "Albahari", "333");
-        CD unborrowedCD = new CD("Coldplay", "Parachutes");
+	        CD cd2 = new CD("Linkin Park", "LP");
+	        cd2.borrow("user3");
+	        cd2.dueDate = LocalDate.now().plusDays(2); // غير متأخر
 
-        // إضافة كل العناصر
-        bookRepo.books.addAll(Arrays.asList(overdueBook, onTimeBook, unborrowedBook));
-        cdRepo.cds.addAll(Arrays.asList(overdueCD, onTimeCD, unborrowedCD));
+	        bookRepo.books.add(book1);
+	        bookRepo.books.add(book2);
 
-        // تهيئة الرصيد
-        fineRepo.balance.put("user1", 5.0);
-        fineRepo.balance.put("user2", 2.0);
-        fineRepo.balance.put("user3", 0.0);
-        fineRepo.balance.put("user4", 1.0);
+	        cdRepo.cds.add(cd1);
+	        cdRepo.cds.add(cd2);
 
-        Librarian librarian = new Librarian(bookRepo, cdRepo, fineRepo);
+	        fineRepo.balance.put("user1", 5.0);
+	        fineRepo.balance.put("user2", 2.0);
 
-        // تحديث الغرامات
-        librarian.updateOverdueFines();
+	        Librarian librarian = new Librarian(bookRepo, cdRepo, fineRepo);
 
-        // Assertions دقيقة
-        assertTrue(fineRepo.getFineBalance("user1") > 5.0); // overdueBook
-        assertTrue(fineRepo.getFineBalance("user2") > 2.0); // overdueCD
-        
-        assertEquals(0.0, fineRepo.getFineBalance("user3")); // onTimeBook
-        assertEquals(1.0, fineRepo.getFineBalance("user4")); // onTimeCD
-    }
+	        librarian.updateOverdueFines();
 
-    @Test
-    void testUpdateOverdueFines_EmptyRepos() {
-        FakeBookRepo bookRepo = new FakeBookRepo();
-        FakeCDRepo cdRepo = new FakeCDRepo();
-        FakeFineRepo fineRepo = new FakeFineRepo();
+	        double user1New = fineRepo.getFineBalance("user1");
+	        double user2New = fineRepo.getFineBalance("user2");
+	        double user3New = fineRepo.getFineBalance("user3");
 
-        Librarian librarian = new Librarian(bookRepo, cdRepo, fineRepo);
+	        System.out.println("user1 fine = " + user1New);
+	        System.out.println("user2 fine = " + user2New);
+	        System.out.println("user3 fine = " + user3New);
 
-        // يجب أن لا يحدث أي استثناء حتى لو لا يوجد كتب/CDs
-        assertDoesNotThrow(librarian::updateOverdueFines);
-    }
+	        // تحقق أن الغرامة زادت بشكل صحيح
+	        assertTrue(user1New > 5.0);  // book1 was overdue 10 days
+	        assertTrue(user2New > 2.0);  // cd1 overdue 4 days
 
-    
+	        // user3 لم يتأخر
+	        assertEquals(0.0, user3New);
+	    }
 }
